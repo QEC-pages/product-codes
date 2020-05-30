27554381fe968547b8a0d469d44a8697e124f009
wzeng002 <wzeng002@ucr.edu>
2020-05-30 16:45:50 -0700

    add .out to git ignore

--- product.sh
+++ product.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=100M
 #SBATCH --time=0-05:00:00     
 #SBATCH --output=log/size8.stdout
 #SBATCH --mail-user=wzeng002@ucr.edu
@@ -50,12 +50,17 @@ index=409
 max_trial=1000000
 na_input=9
 
+
+logfile=log/index${index}-size${na_input}.log
+
+echo check logfile: $logfile
+
 #index=221 #218-221 - for reduced code.
 #index=200-217 for concatenation
 #index=1-112 #for hypergraph
 
 make counter_concatenation.out
-cp counter_concatenation.out .run$index.sh
+cp counter_concatenation.out .product$index.out
 
 #add index by 1 while rerun this script
 #the number of simultaneous process is limited by max_process.
@@ -72,35 +77,36 @@ case `hostname` in
 esac
 
 
-echo start job on `hostname` size$na_input run$index $max_process/$max_trial `date`
-echo one dot means 1000 test
+echo start job on `hostname` size$na_input run$index $max_process/$max_trial `date` > $logfile
+echo one dot means 1000 test >> $logfile
 (( i = 0 ))
 while (( i < max_trial ))
 do
-    num_process=`pgrep -c counter_con`
+    num_process=`pgrep -c product`
     for (( j = num_process ; j < max_process ; j++ ))
     do
 	    title=$folder/trial$index-$i
 #	    ./random_concatenation.out 1 $title    >>data/result/result$index-$i.log &
 	    #./counter_concatenation.out mode=1 title=$title debug=0 &
-	    ./.run$index.sh  mode=1 title=$title debug=0 na_input=$na_input &
+	    ./.product$index.out  mode=1 title=$title debug=0 na_input=$na_input  >> $logfile &
 	    #>>data/result/result$index-$i.log &	    
 	    #1 for generate random code
 	    #./random_concatenation.out  >>data/result/result$index-$i.log &
 	    if (( i % 1000 == 0 )) then 
-		echo -n "." 
+		echo -n "." >> $logfile
 		#the following is a bit strange, and show different output using less and cat
 		#echo -ne ${max_trial} $title `date` \\r
+		#echo ${max_trial} $title `date` >> $logfile
 	    fi
 	    (( i++ ))
     done
 
-    sleep 0.1
+    sleep 0.3
 
 done
 
 
 wait
-echo 
-echo finish job on `hostname` $index $max_process/$max_trial `date`
+echo >> $logfile
+echo finish job on `hostname` $index $max_process/$max_trial `date` >> $logfile
 
