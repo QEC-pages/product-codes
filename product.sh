48dc90d96d9adf36308463131af30e7ac6fed8ca
wzeng002 <wzeng002@ucr.edu>
2020-06-01 10:31:24 -0700

    use 60 to leave room for srun

--- product.sh
+++ product.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=60
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=0-2:00:00     
 #SBATCH --output=log/product.stdout
