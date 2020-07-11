788f6f8a6c21acfcc8e1b23a2ef42f53a7afc8d1
wzeng002 <wzeng002@ucr.edu>
2020-07-11 16:37:58 -0700

    add matrixces for cases

---
+++ report/matrices/trial560-A-7-3-3803-3-2131-B-7-3-3767-3-2386Gax.mm
@@ -0,0 +1,14 @@
+%%MatrixMarket matrix coordinate real general
+3 7 12
+1 1          1
+1 4          1
+1 5          1
+1 6          1
+2 2          1
+2 4          1
+2 5          1
+2 7          1
+3 3          1
+3 4          1
+3 6          1
+3 7          1
